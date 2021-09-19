b6b384f6a708cbfadd011e7f8fca914b6198f65d
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-19 23:30:45 +0200

    Add keywords-arr and keywords-obj to storage

--- popup.js
+++ popup.js
@@ -4,5 +4,13 @@ const uploadButton = document.querySelector("#save-keywords");
 uploadButton.addEventListener("click", function handleUploadClick(event) {
   const keywords = inputField.value;
   chrome.storage.sync.set({ "keywords-input": keywords });
-  // read with `chrome.storage.sync.get(console.log);`
+
+  {
+    const keywordArrRaw = keywords.split(",");
+    const keywordArr = keywordArrRaw.map((k) => k.trim());
+    const keywordObj = {};
+
+    chrome.storage.sync.set({ "keywords-arr": keywordArr });
+    chrome.storage.sync.set({ "keywords-obj": keywordObj });
+  }
 });
