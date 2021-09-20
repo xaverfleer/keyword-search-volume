47a1e274dd1cb4e22e642e88ecf5cf316104cd40
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-20 17:51:51 +0200

    Add fumctionality to loop through keywords

--- background.js
+++ background.js
@@ -1,5 +1,48 @@
 console.log("Hello background");
 
-chrome.tabs.create({
-  url: "https://www.google.com/search?q=allintitle:Hello World",
-});
+chrome.storage.sync.set({ analyze: false });
+let analyzing, keywordsArr, keywordsObj, currentKw;
+
+init();
+
+function handleStorageUpdates(changes) {
+  if (changes.analyze) {
+    analyzing = changes.analyze.newValue;
+    if (analyzing) analyze();
+  }
+
+  if (changes["keywords-arr"]) keywordsArr = changes["keywords-arr"].newValue;
+
+  if (changes["keywords-obj"]) keywordsObj = changes["keywords-obj"].newValue;
+  if (keywordsObj[currentKw]) {
+    currentKw = "";
+    analyze();
+  }
+}
+
+function init() {
+  chrome.storage.sync.get(["keywords-arr", "keywords-obj"], (items) => {
+    keywordsArr = items["keywords-arr"];
+    keywordsObj = items["keywords-obj"];
+  });
+  chrome.storage.onChanged.addListener(handleStorageUpdates);
+}
+
+function analyze() {
+  let found = false;
+  for (let kw of keywordsArr) {
+    if (!keywordsObj[kw]) {
+      analyzeKw(kw);
+      found = true;
+      break;
+    }
+  }
+  chrome.storage.sync.set({ analyze: false });
+}
+
+function analyzeKw(kw) {
+  currentKw = kw;
+  chrome.tabs.create({
+    url: `https://www.google.com/search?q=allintitle:${kw}`,
+  });
+}
--- popup.js
+++ popup.js
@@ -3,88 +3,86 @@ console.log("hello popup");
 
 const elems = {};
 
-const storageKeys = {
-  kwdsInput: "keywords-input",
-  kwdsArr: "keywords-arr",
-  kwdsObj: "keywords-obj",
-};
-
 chrome.storage.onChanged.addListener(handleStorageUpdates);
 
 {
-  function init() {
-    elems.resultsTableBody =
-      elems.resultsTableBody || document.querySelector(".results-table__tbody");
+  setTimeout(init);
 
-    if (elems.resultsTableBody) {
+  function init() {
+    const isLoaded = document.querySelector(".results-table__tbody");
+
+    if (isLoaded) {
       initElems();
-      elems.button.addEventListener("click", handleUploadClick);
+      elems.updateKwdsButton.addEventListener("click", handleUploadClick);
+      elems.startAnalysis.addEventListener("click", handleStartAnalisisClick);
 
       initGui();
     } else {
       setTimeout(init);
     }
   }
-  setTimeout(init);
 
   function initElems() {
     elems.input = document.querySelector("#keywords");
-    elems.button = document.querySelector("#save-keywords");
+    elems.updateKwdsButton = document.querySelector("#update-keywords");
     elems.resultsTableBody = document.querySelector(".results-table__tbody");
+    elems.startAnalysis = document.querySelector("#start-analysis");
   }
 }
 
 function handleUploadClick(event) {
   const keywords = elems.input.value;
-  chrome.storage.sync.set({ [storageKeys.kwdsInput]: keywords });
+  chrome.storage.sync.set({ "keywords-input": keywords });
 
   {
     const keywordArrRaw = keywords.split(",");
     const keywordArr = keywordArrRaw.map((k) => k.trim());
     const keywordObj = {};
 
-    chrome.storage.sync.set({ [storageKeys.kwdsArr]: keywordArr });
-    chrome.storage.sync.set({ [storageKeys.kwdsObj]: keywordObj });
+    chrome.storage.sync.set({ ["keywords-arr"]: keywordArr });
+    chrome.storage.sync.set({ ["keywords-obj"]: keywordObj });
   }
 }
 
+function handleStartAnalisisClick() {
+  chrome.storage.sync.set({ analyze: true });
+}
+
 function handleStorageUpdates(changes) {
-  const didKeywordsChange = changes[storageKeys.kwdsInput]?.newValue;
-  if (didKeywordsChange) {
+  if (changes["keywords-input"]) {
     updateTable();
   }
+
+  if (changes["keywords-arr"]) chrome.storage.sync.set({ analyze: false });
 }
 
 function initGui() {
   updateTable();
-  chrome.storage.sync.get([storageKeys.kwdsInput], (items) => {
-    elems.input.value = items[storageKeys.kwdsInput];
+  chrome.storage.sync.get("keywords-input", (items) => {
+    elems.input.value = items["keywords-input"];
   });
 }
 
 function updateTable() {
-  chrome.storage.sync.get(
-    [storageKeys.kwdsArr, storageKeys.kwdsObj],
-    function (items) {
-      let keywordsArr = items[storageKeys.kwdsArr];
-      let keywordsObj = items[storageKeys.kwdsObj];
+  chrome.storage.sync.get(["keywords-arr", "keywords-obj"], function (items) {
+    let keywordsArr = items["keywords-arr"];
+    let keywordsObj = items["keywords-obj"];
 
-      for (let kw of keywordsArr) {
-        let tr = document.createElement("tr");
+    for (let kw of keywordsArr) {
+      let tr = document.createElement("tr");
 
-        let tdKey = document.createElement("td");
-        tdKey.innerText = kw;
-        tr.appendChild(tdKey);
+      let tdKey = document.createElement("td");
+      tdKey.innerText = kw;
+      tr.appendChild(tdKey);
 
-        let searchResults = keywordsObj[kw] || "?";
-        let tdRsult = document.createElement("td");
-        tdRsult.innerText = searchResults;
-        tr.appendChild(tdRsult);
+      let searchResults = keywordsObj[kw] || "?";
+      let tdRsult = document.createElement("td");
+      tdRsult.innerText = searchResults;
+      tr.appendChild(tdRsult);
 
-        elems.resultsTableBody.appendChild(tr);
-      }
+      elems.resultsTableBody.appendChild(tr);
     }
-  );
+  });
 
   elems.resultsTableBody.innerHTML = "";
 }
