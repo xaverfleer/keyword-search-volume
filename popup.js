dc79c1f67fcd28d1f1886d117991663602ed43a8
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-20 11:20:24 +0200

    Add initialization phase

--- popup.js
+++ popup.js
@@ -1,10 +1,7 @@
+"use strict";
 console.log("hello popup");
 
-const elems = {
-  input: document.querySelector("#keywords"),
-  button: document.querySelector("#save-keywords"),
-  resultsTableBody: document.querySelector(".results-table__tbody"),
-};
+const elems = {};
 
 const storageKeys = {
   kwdsInput: "keywords-input",
@@ -12,7 +9,32 @@ const storageKeys = {
   kwdsObj: "keywords-obj",
 };
 
-elems.button.addEventListener("click", function handleUploadClick(event) {
+chrome.storage.onChanged.addListener(handleStorageUpdates);
+
+{
+  function init() {
+    elems.resultsTableBody =
+      elems.resultsTableBody || document.querySelector(".results-table__tbody");
+
+    if (elems.resultsTableBody) {
+      initElems();
+      elems.button.addEventListener("click", handleUploadClick);
+
+      updateTable();
+    } else {
+      setTimeout(init);
+    }
+  }
+  setTimeout(init);
+
+  function initElems() {
+    elems.input = document.querySelector("#keywords");
+    elems.button = document.querySelector("#save-keywords");
+    elems.resultsTableBody = document.querySelector(".results-table__tbody");
+  }
+}
+
+function handleUploadClick(event) {
   const keywords = elems.input.value;
   chrome.storage.sync.set({ [storageKeys.kwdsInput]: keywords });
 
@@ -24,38 +46,38 @@ elems.button.addEventListener("click", function handleUploadClick(event) {
     chrome.storage.sync.set({ [storageKeys.kwdsArr]: keywordArr });
     chrome.storage.sync.set({ [storageKeys.kwdsObj]: keywordObj });
   }
-});
+}
 
-chrome.storage.onChanged.addListener(function (changes) {
-  elems.resultsTableBody =
-    elems.resultsTableBody || document.querySelector(".results-table__tbody");
-
+function handleStorageUpdates(changes) {
   const didKeywordsChange = changes[storageKeys.kwdsInput]?.newValue;
   if (didKeywordsChange) {
-    let keywordsArr;
-    chrome.storage.sync.get(
-      [storageKeys.kwdsArr, storageKeys.kwdsObj],
-      function (items) {
-        keywordsArr = items[storageKeys.kwdsArr];
-        keywordsObj = items[storageKeys.kwdsObj];
-
-        for (kw of keywordsArr) {
-          let tr = document.createElement("tr");
-
-          let tdKey = document.createElement("td");
-          tdKey.innerText = kw;
-          tr.appendChild(tdKey);
-
-          let searchResults = keywordsObj[kw] || "?";
-          let tdRsult = document.createElement("td");
-          tdRsult.innerText = searchResults;
-          tr.appendChild(tdRsult);
-
-          elems.resultsTableBody.appendChild(tr);
-        }
-      }
-    );
-
-    elems.resultsTableBody.innerHTML = "";
+    updateTable();
   }
-});
+}
+
+function updateTable() {
+  chrome.storage.sync.get(
+    [storageKeys.kwdsArr, storageKeys.kwdsObj],
+    function (items) {
+      let keywordsArr = items[storageKeys.kwdsArr];
+      let keywordsObj = items[storageKeys.kwdsObj];
+
+      for (let kw of keywordsArr) {
+        let tr = document.createElement("tr");
+
+        let tdKey = document.createElement("td");
+        tdKey.innerText = kw;
+        tr.appendChild(tdKey);
+
+        let searchResults = keywordsObj[kw] || "?";
+        let tdRsult = document.createElement("td");
+        tdRsult.innerText = searchResults;
+        tr.appendChild(tdRsult);
+
+        elems.resultsTableBody.appendChild(tr);
+      }
+    }
+  );
+
+  elems.resultsTableBody.innerHTML = "";
+}
