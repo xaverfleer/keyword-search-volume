cdd0889eee03254c87d09619ff0f5dd480fc6176
Xaver Fleer <xaver.fleer@gmail.com>
2021-09-22 08:50:26 +0200

    Handle 'did not match any documents'

--- content-script.js
+++ content-script.js
@@ -1,6 +1,7 @@
 const elems = {
   input: document.querySelector('input[aria-label="Search"]'),
   results: document.querySelector("#result-stats"),
+  heading: document.querySelector('p[role="heading"]'),
   submit: document.querySelector('button[aria-label="Google Search"]'),
 };
 
@@ -9,7 +10,11 @@ const searchTerm = elems.input.value.split(/^allintitle:/)[1];
 if (searchTerm) {
   var resultsNum = elems.results
     ? +elems.results.innerText.match(/[0-9’]+/)[0].replace(/’/g, "")
-    : 0;
+    : elems.heading?.elems.heading.innerText.includes(
+        "did not match any documents"
+      )
+    ? 0
+    : undefined;
 
   chrome.storage.sync.get(["keywords-obj"], (items) => {
     const kwObj = items["keywords-obj"] || {};
